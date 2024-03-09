fd98bd25bbd5be16da2505b56b967114dd196bde
agostinoluca <agostino.luca89@gmail.com>
2024-03-09 16:45:30 +0100

    find error in function

--- display_password.php
+++ display_password.php
@@ -1,16 +1,25 @@
 <?php
 session_start();
 require_once __DIR__ . '/html_head.php';
+$newPassword = $_SESSION['password'];
 ?>
 
 <body class="bg-secondary">
     <div class="d-flex justify-content-center mt-5 text-center p-5">
-        <div class="card text-bg-success mb-3">
-            <div class="card-header text-dark fw-bolder fs-4 ">Your new strong password:</div>
-            <div class="card-body">
-                <h5 class="card-title p-2 fs-3"><?php echo $_SESSION['password'] ?></h5>
+        <?php if (strlen($newPassword) < 4) : ?>
+            <div class="alert alert-primary fw-bold " role="alert">
+                <p class="text-danger">The password creation failed.</p>
+                <p>Please ensure you have filled in all required fields.</p>
             </div>
-        </div>
+        <?php endif ?>
+        <?php if (strlen($newPassword) > 4) : ?>
+            <div class="card text-bg-success mb-3">
+                <div class="card-header text-dark fw-bolder fs-4 ">Your new strong password:</div>
+                <div class="card-body">
+                    <h5 class="card-title p-2 fs-3"><?php echo $newPassword ?></h5>
+                </div>
+            </div>
+        <?php endif ?>
     </div>
 </body>
 
---
+++ error.md
@@ -0,0 +1,5 @@
+# ERRORE NELLA LOGICA
+
+La password generata risulta essere inputNumber + 1.
+La logica tiene conto dell'index 0 nel ciclo while, dunque se il numero inserito è 4 ciclerà 01234.
+Vorrei risolvere senza stravolgere la logica già inserita.
